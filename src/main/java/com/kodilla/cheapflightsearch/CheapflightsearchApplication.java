cdc55127506f32f32e1df30a48befe936abd3ff6
Seweryn <sew_k@wp.pl>
2023-07-10 22:03:32 +0200

    First commit

---
+++ src/main/java/com/kodilla/cheapflightsearch/CheapflightsearchApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.cheapflightsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheapflightsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheapflightsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodilla/cheapflightsearch/CheapflightsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.cheapflightsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheapflightsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
