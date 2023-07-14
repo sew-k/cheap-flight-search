b510546de78123063059ba8c5d6de27b6f42d144
Seweryn <sew_k@wp.pl>
2023-07-14 22:02:40 +0200

    Fixes to build war file

--- src/main/java/com/kodilla/cheapflightsearch/CheapflightsearchApplication.java
+++ src/main/java/com/kodilla/cheapflightsearch/CheapflightsearchApplication.java
@@ -2,12 +2,17 @@ package com.kodilla.cheapflightsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CheapflightsearchApplication {
+public class CheapflightsearchApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CheapflightsearchApplication.class, args);
 	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CheapflightsearchApplication.class);
+	}
 }
