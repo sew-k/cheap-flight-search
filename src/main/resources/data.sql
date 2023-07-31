f6667c6cd20b52ccc1aefa46c86ed32cc5be4453
Seweryn <sew_k@wp.pl>
2023-07-31 16:04:57 +0200

    Added initialisation of basic airports data set

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT IGNORE INTO airports (airport_id, country, city, iata_code)
+VALUES  (18, "Portugal", "Lisbon", "LIS"),
+        (20, "Portugal", "Porto", "OPO"),
+        (21, "Portugal", "Porto", "OPO"),
+        (22, "Poland", "Wroclaw", "WRO"),
+        (23, "Poland", "Warsaw", "WAW"),
+        (24, "Poland", "Warsaw-Modlin", "WMI"),
+        (25, "Poland", "Gdansk", "GDN"),
+        (30, "Spain", "Palma de Mallorca", "PMI"),
+        (31, "Spain", "Malaga", "AGP"),
+        (40, "Greece", "Korfu", "CFU"),
+        (41, "Greece", "Rhodes", "RHO"),
+        (42, "Greece", "Chania", "CHQ"),
+        (42, "Greece", "Heraklion", "HER");
