b344aa9052b7ba2e8de1c12a5d62ef4ab2c4e0d4
Seweryn <sew_k@wp.pl>
2023-08-01 11:51:46 +0200

    Weather: Controller implementation, Service minor changes

--- src/main/java/com/kodilla/cheapflightsearch/controller/WeatherController.java
+++ src/main/java/com/kodilla/cheapflightsearch/controller/WeatherController.java
@@ -1,4 +1,18 @@
 package com.kodilla.cheapflightsearch.controller;
 
+import com.kodilla.cheapflightsearch.service.WeatherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/cheapflightsearch/weather")
+@RequiredArgsConstructor
+@CrossOrigin("*")
 public class WeatherController {
+    private final WeatherService weatherService;
+    @GetMapping("/{city}")
+    public ResponseEntity<Float> getWeatherByCity(@PathVariable("city") String city) {
+        return ResponseEntity.ok(weatherService.getWeather(city).getTemperature());
+    }
 }
--- src/main/java/com/kodilla/cheapflightsearch/service/WeatherService.java
+++ src/main/java/com/kodilla/cheapflightsearch/service/WeatherService.java
@@ -2,7 +2,6 @@ package com.kodilla.cheapflightsearch.service;
 
 import com.kodilla.cheapflightsearch.config.OpenWeatherConfig;
 import com.kodilla.cheapflightsearch.domain.weather.Weather;
-import com.kodilla.cheapflightsearch.domain.weather.WeatherDto;
 import com.kodilla.cheapflightsearch.mapper.WeatherMapper;
 import com.kodilla.cheapflightsearch.webclient.weather.WeatherClient;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +12,7 @@ import org.springframework.stereotype.Service;
 public class WeatherService {
     private final WeatherClient weatherClient;
     private final WeatherMapper weatherMapper;
-    public Weather getWeather(String city) { //TODO - sparametryzować to zapytanie, dodać klasę Config i Controller
+    public Weather getWeather(String city) {
         return weatherMapper.mapToCurrentWeather(weatherClient.getWeatherForCity(
                 city,
                 OpenWeatherConfig.CALL_FOR_CURRENT_WEATHER,
