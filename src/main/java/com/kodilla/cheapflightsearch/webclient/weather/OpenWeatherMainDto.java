e0ab2e30fa538484ac75b0cec8c0558fc48ac9b2
Seweryn <sew_k@wp.pl>
2023-08-02 17:17:40 +0200

    Added missing annotations

--- src/main/java/com/kodilla/cheapflightsearch/webclient/weather/OpenWeatherMainDto.java
+++ src/main/java/com/kodilla/cheapflightsearch/webclient/weather/OpenWeatherMainDto.java
@@ -1,10 +1,13 @@
 package com.kodilla.cheapflightsearch.webclient.weather;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class OpenWeatherMainDto {
     private float temp;
 }
