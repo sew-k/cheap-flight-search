cf3ab93630d05f68298dc845b4eada9e3dc325d7
Seweryn <sew_k@wp.pl>
2023-09-01 21:22:32 +0200

    Security: fixed ability to run after logging in as administrator

--- src/main/java/com/kodilla/cheapflightsearch/service/UserDetailsServiceImpl.java
+++ src/main/java/com/kodilla/cheapflightsearch/service/UserDetailsServiceImpl.java
@@ -3,12 +3,14 @@ package com.kodilla.cheapflightsearch.service;
 import com.kodilla.cheapflightsearch.domain.user.User;
 import com.kodilla.cheapflightsearch.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,18 +18,26 @@ import java.util.Optional;
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     UserRepository userRepository;
+    private List<GrantedAuthority> authorities = new ArrayList<>();
+    private GrantedAuthority authority;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> user = userRepository.findByUsername(username);
         if (!user.isPresent()) {
             throw new UsernameNotFoundException("User not found");
+        } else {
+            authorities.add(getUserSimpleGrantedAuthority(user));
+            return new org.springframework.security.core.userdetails.User(
+                    user.get().getUsername(),
+                    user.get().getPassword(),
+                    authorities
+            );
         }
-        return new org.springframework.security.core.userdetails.User(
-                user.get().getUsername(),
-                user.get().getPassword(),
-                List.of(new SimpleGrantedAuthority(user.get().getRole().toString()))
-        );
+    }
+
+    private SimpleGrantedAuthority getUserSimpleGrantedAuthority(Optional<User> user) {
+        return new SimpleGrantedAuthority("ROLE_" + user.get().getRole().toString());
     }
 }
 
