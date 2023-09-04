43a6bae5ebf707033b778d7b7c4e6c7971833eeb
Seweryn <sew_k@wp.pl>
2023-09-04 19:55:44 +0200

    Renamed: Fixed incorrect name.

--- src/main/java/com/kodilla/cheapflightsearch/CheapFlightSearchApplication.java
+++ src/main/java/com/kodilla/cheapflightsearch/CheapFlightSearchApplication.java
@@ -6,17 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @OpenAPIDefinition
+@EnableScheduling
 @EnableAspectJAutoProxy
 @SpringBootApplication
-public class CheapflightsearchApplication extends SpringBootServletInitializer {
+public class CheapFlightSearchApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CheapflightsearchApplication.class, args);
+		SpringApplication.run(CheapFlightSearchApplication.class, args);
 	}
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(CheapflightsearchApplication.class);
+		return application.sources(CheapFlightSearchApplication.class);
 	}
 }
