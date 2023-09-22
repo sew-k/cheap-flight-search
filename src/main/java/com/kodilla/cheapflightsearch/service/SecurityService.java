10e7c42376e5b29b2aac100a95fe7c44470eed42
Seweryn <sew_k@wp.pl>
2023-09-22 20:21:41 +0200

    SecurityService: Minor changes.

--- src/main/java/com/kodilla/cheapflightsearch/service/SecurityService.java
+++ src/main/java/com/kodilla/cheapflightsearch/service/SecurityService.java
@@ -11,7 +11,7 @@ public class SecurityService {
     private final AuthenticationContext authenticationContext;
 
     public UserDetails getAuthenticatedUser() {
-        return authenticationContext.getAuthenticatedUser(UserDetails.class).get();
+        return authenticationContext.getAuthenticatedUser(UserDetails.class).orElse(null);
     }
 
     public void logout() {
