994356e54ce99d5de6052bd42e69b09064909be3
Seweryn <sew_k@wp.pl>
2023-09-29 20:49:03 +0200

    ItineraryRepository: Fixed method args in 'existsByItineraryMark'.

--- src/main/java/com/kodilla/cheapflightsearch/repository/ItineraryRepository.java
+++ src/main/java/com/kodilla/cheapflightsearch/repository/ItineraryRepository.java
@@ -6,21 +6,27 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+
 @Repository
 @Transactional
 public interface ItineraryRepository extends CrudRepository<Itinerary, Long> {
     @Override
     List<Itinerary> findAll();
+
     @Override
     Itinerary save(Itinerary itinerary);
+
     @Override
     Optional<Itinerary> findById(Long id);
+
     @Override
     void deleteById(Long id);
-    boolean existsByItineraryMark(Itinerary itinerary);
+
+    boolean existsByItineraryMark(String itineraryMark);
+
     boolean existsById(Long id);
+
     List<Itinerary> findByTripPlan(TripPlan tripPlan);
 }
