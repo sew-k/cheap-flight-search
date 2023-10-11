9f0808ef0a6fd2936ef9a234d1724d1804fd6cc1
Seweryn <sew_k@wp.pl>
2023-10-11 17:15:24 +0200

    SecurityService: Code cleaning - added method 'isCurrentUserAllowed'.

--- src/main/java/com/kodilla/cheapflightsearch/service/SecurityService.java
+++ src/main/java/com/kodilla/cheapflightsearch/service/SecurityService.java
@@ -5,10 +5,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class SecurityService {
     private final AuthenticationContext authenticationContext;
+
+    public boolean isCurrentUserAllowed() {
+        Optional<UserDetails> loggedUserDetails = Optional.ofNullable(getAuthenticatedUser());
+        if (loggedUserDetails.isPresent()) {
+            return (loggedUserDetails.get().isAccountNonExpired()
+                    && loggedUserDetails.get().isAccountNonLocked()
+                    && loggedUserDetails.get().isCredentialsNonExpired());
+        } else {
+            return false;
+        }
+    }
 
     public UserDetails getAuthenticatedUser() {
         return authenticationContext.getAuthenticatedUser(UserDetails.class).orElse(null);
