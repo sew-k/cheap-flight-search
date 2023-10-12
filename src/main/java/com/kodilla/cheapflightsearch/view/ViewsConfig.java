3d21216c56095049bf66bc73ded6daca415ff3f9
Seweryn <sew_k@wp.pl>
2023-10-12 20:19:37 +0200

    Vaadin: Added ViewsConfig class to store config data for view layer.

---
+++ src/main/java/com/kodilla/cheapflightsearch/view/ViewsConfig.java
@@ -0,0 +1,6 @@
+package com.kodilla.cheapflightsearch.view;
+
+public class ViewsConfig {
+    public static final float MINIMUM_MENU_BUTTONS_WIDTH = 200f;
+    public static final float MINIMUM_OPTION_BUTTONS_WIDTH = 150f;
+}
