809197daece72c6e1bb0af6756b4db0bb4cfd13e
Seweryn <sew_k@wp.pl>
2023-10-13 21:28:53 +0200

    Vaadin: Added variables to ViewsConfig.

--- src/main/java/com/kodilla/cheapflightsearch/view/ViewsConfig.java
+++ src/main/java/com/kodilla/cheapflightsearch/view/ViewsConfig.java
@@ -4,4 +4,7 @@ public class ViewsConfig {
     public static final float MINIMUM_MENU_BUTTONS_WIDTH = 200f;
     public static final float MINIMUM_OPTION_BUTTONS_WIDTH = 150f;
     public static final float MINIMUM_SUBMIT_COMPONENTS_WIDTH = 300f;
+    public static final float MINIMUM_AIRPORT_COMPONENTS_WIDTH = 300f;
+    public static final float MINIMUM_DATE_COMPONENTS_WIDTH = 150f;
+    public static final float ADULTS_COMPONENTS_WIDTH = 100f;
 }
