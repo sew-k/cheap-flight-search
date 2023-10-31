5bf3b8eab704204faed1b124d6e5213eefaf8fc1
Seweryn <sew_k@wp.pl>
2023-10-31 18:20:45 +0100

    RouteServiceTestSuite: First version of test suite.

--- src/test/java/com/kodilla/cheapflightsearch/service/RouteServiceTestSuite.java
+++ src/test/java/com/kodilla/cheapflightsearch/service/RouteServiceTestSuite.java
@@ -1,13 +1,39 @@
 package com.kodilla.cheapflightsearch.service;
 
+import com.kodilla.cheapflightsearch.domain.trip.Route;
+import com.kodilla.cheapflightsearch.repository.RouteRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class RouteServiceTestSuite {
+    @InjectMocks
+    RouteService routeService;
+    @Mock
+    RouteRepository routeRepository;
 
     @Test
-    void getRoutes() {
+    void testGetRoutes() {
+        //Given
+        Route route1 = new Route();
+        Route route2 = new Route();
+        when(routeRepository.findAll()).thenReturn(List.of(route1, route2));
+
+        //When
+        List<Route> fetchedRoutes = routeService.getRoutes();
+
+        //Then
+        verify(routeRepository, atLeastOnce()).findAll();
+        assertFalse(fetchedRoutes.isEmpty());
+        assertEquals(2, fetchedRoutes.size());
     }
 
     @Test
@@ -36,5 +62,13 @@ class RouteServiceTestSuite {
 
     @Test
     void getFavouriteRoutes() {
+    }
+
+    @Test
+    void switchFavourite() {
+    }
+
+    @Test
+    void getRoutesByUserId() {
     }
 }
