efba71df304ce794abe55ef0504d7a20953335d1
Seweryn <sew_k@wp.pl>
2023-11-02 19:42:03 +0100

    RouteServiceTestSuite: Added missed unit tests.

--- src/test/java/com/kodilla/cheapflightsearch/service/RouteServiceTestSuite.java
+++ src/test/java/com/kodilla/cheapflightsearch/service/RouteServiceTestSuite.java
@@ -1,6 +1,7 @@
 package com.kodilla.cheapflightsearch.service;
 
 import com.kodilla.cheapflightsearch.domain.trip.Route;
+import com.kodilla.cheapflightsearch.domain.user.User;
 import com.kodilla.cheapflightsearch.exception.RouteNotFoundException;
 import com.kodilla.cheapflightsearch.repository.RouteRepository;
 import org.junit.jupiter.api.Test;
@@ -64,34 +65,134 @@ class RouteServiceTestSuite {
     }
 
     @Test
-    void deleteRoute() {
+    void deleteRoute() throws Exception {
+        //Given
+        Route route5 = new Route();
+        Long id = 5L;
+        when(routeRepository.findById(id)).thenReturn(Optional.of(route5));
+
+        //When
+        routeService.deleteRoute(id);
+
+        //Then
+        assertDoesNotThrow(() -> routeService.deleteRoute(id));
+        verify(routeRepository, atLeastOnce()).deleteById(id);
     }
 
     @Test
-    void updateRoute() {
+    void deleteRoute_notExisting() throws Exception {
+        //Given
+        Long id = 6L;
+
+        //When & Then
+        assertThrows(RouteNotFoundException.class, () -> routeService.deleteRoute(id));
+    }
+
+    @Test
+    void updateRoute() throws Exception {
+        //Given
+        Route route7 = new Route();
+        Long idToUpdate = 7L;
+        Route route8 = new Route();
+        Long idUpdated = 8L;
+        when(routeRepository.findById(idToUpdate)).thenReturn(Optional.of(route7));
+        when(routeRepository.save(route7)).thenReturn(route8);
+
+        //When
+        Route updatedRoute = routeService.updateRoute(idToUpdate, route7);
+
+        //Then
+        assertDoesNotThrow(() -> routeService.updateRoute(idToUpdate, route7));
+        verify(routeRepository, atLeastOnce()).save(route7);
+        assertEquals(route8, updatedRoute);
+    }
+
+    @Test
+    void updateeRoute_notExisting() throws Exception {
+        //Given
+        Long id = 9L;
+
+        //When & Then
+        assertThrows(RouteNotFoundException.class, () -> routeService.updateRoute(id, new Route()));
     }
 
     @Test
     void createRoute() {
-    }
+        //Given
+        Route route10 = new Route();
+        Long id = 10L;
+        when(routeRepository.save(route10)).thenReturn(route10);
 
-    @Test
-    void getRouteByOrigin() {
-    }
+        //When
+        Route createdRoute = routeService.createRoute(route10);
 
-    @Test
-    void getRouteByDestination() {
+        //Then
+        verify(routeRepository, atLeastOnce()).save(route10);
+        assertEquals(route10, createdRoute);
     }
 
     @Test
     void getFavouriteRoutes() {
+        //Given
+        Route route11 = new Route();
+        route11.setFavourite(true);
+        Route route12 = new Route();
+        route12.setFavourite(false);
+        Route route13 = new Route();
+        route13.setFavourite(true);
+        List<Route> routes = List.of(route11, route12, route13);
+        when(routeRepository.findAll()).thenReturn(routes);
+
+        //When
+        List<Route> fetchedRoutes = routeService.getFavouriteRoutes();
+
+        //Then
+        verify(routeRepository, atLeastOnce()).findAll();
+        assertFalse(fetchedRoutes.isEmpty());
+        assertEquals(2, fetchedRoutes.size());
+        assertTrue(fetchedRoutes.contains(route11));
+        assertTrue(fetchedRoutes.contains(route13));
     }
 
     @Test
     void switchFavourite() {
+        //Given
+        Route route14 = new Route();
+        route14.setFavourite(false);
+        when(routeRepository.save(route14)).thenReturn(route14);
+
+        //When
+        routeService.switchFavourite(route14);
+
+        //Then
+        verify(routeRepository, atLeastOnce()).save(route14);
+        assertTrue(route14.isFavourite());
     }
 
     @Test
     void getRoutesByUserId() {
+        //Given
+        User user1 = new User();
+        user1.setUserId(19L);
+        Route route15 = new Route();
+        route15.setUser(user1);
+        Route route16 = new Route();
+        route16.setUser(user1);
+        User user2 = new User();
+        user2.setUserId(20L);
+        Route route17 = new Route();
+        route17.setUser(user2);
+        List<Route> routes = List.of(route15, route16, route17);
+        when(routeRepository.findAll()).thenReturn(routes);
+
+        //When
+        List<Route> fetchedRoutes = routeService.getRoutesByUserId(19L);
+
+        //Then
+        verify(routeRepository, atLeastOnce()).findAll();
+        assertFalse(fetchedRoutes.isEmpty());
+        assertEquals(2, fetchedRoutes.size());
+        assertTrue(fetchedRoutes.contains(route15));
+        assertTrue(fetchedRoutes.contains(route16));
     }
 }
