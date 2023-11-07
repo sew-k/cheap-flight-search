585b895973941fd96b9238bab6ae62dced23637b
Seweryn <sew_k@wp.pl>
2023-11-07 20:54:14 +0100

    ItineraryService: Changes connected with creating TripPlanService (moved methods).

--- src/main/java/com/kodilla/cheapflightsearch/service/ItineraryService.java
+++ src/main/java/com/kodilla/cheapflightsearch/service/ItineraryService.java
@@ -7,7 +7,6 @@ import com.kodilla.cheapflightsearch.domain.skyscanner.Itinerary;
 
 import com.kodilla.cheapflightsearch.domain.trip.Route;
 import com.kodilla.cheapflightsearch.domain.trip.TripPlan;
-import com.kodilla.cheapflightsearch.exception.TripPlanNotFoundException;
 import com.kodilla.cheapflightsearch.mapper.SkyscannerMapper;
 import com.kodilla.cheapflightsearch.mapper.TripPlanMapper;
 import com.kodilla.cheapflightsearch.repository.ItineraryRepository;
@@ -192,98 +191,5 @@ public class ItineraryService {
             }
         }
         return resultList;
-    }
-
-    public List<TripPlan> getTripPlans() {
-        return tripPlanRepository.findAll();
-    }
-
-    public List<TripPlan> getTripPlansByUserId(Long userId) {
-        return tripPlanRepository.findAll().stream()
-                .filter(t -> t.getUser().getUserId().equals(userId))
-                .collect(Collectors.toList());
-    }
-
-    public List<TripPlan> getTripPlansByUser(User user) {
-        return tripPlanRepository.findByUser(user);
-    }
-
-    public void deleteTripPlan(Long id) throws TripPlanNotFoundException {
-        Optional<TripPlan> tripPlanToDelete = tripPlanRepository.findById(id);
-        if (tripPlanToDelete.isPresent()) {
-            itineraryRepository.findByTripPlan(tripPlanToDelete.get()).stream()
-                    .forEach(i -> {
-                        i.setTripPlan(null);
-                        itineraryRepository.save(i);
-                    });
-            tripPlanRepository.deleteById(id);
-        } else {
-            throw new TripPlanNotFoundException();
-        }
-    }
-
-    public TripPlan updateTripPlan(Long id, TripPlan tripPlan) throws TripPlanNotFoundException {
-        if (tripPlanRepository.findById(id).isPresent()) {
-            return tripPlanRepository.save(tripPlan);
-        } else {
-            throw new TripPlanNotFoundException();
-        }
-    }
-
-    public TripPlan createTripPlan(TripPlan tripPlan) {
-        User user = tripPlan.getUser();
-        TripPlan updatedTripPlan = tripPlan;
-        if (!tripPlanRepository.findByUser(user).contains(tripPlan)) {
-            updatedTripPlan = tripPlanRepository.save(tripPlan);
-        }
-        return updatedTripPlan;
-    }
-
-    public List<TripPlan> createTripPlansFromFavouriteRoutesAndHolidayPlans(User currentUser, int adults) {
-        List<Route> routes = routeRepository.findByUser(currentUser);
-        List<HolidayPlan> holidayPlans = calendarService.getHolidayPlansByUser(currentUser); //TODO maybe is better to add relations to user from calendar side to use repository here;
-        List<TripPlan> currentUserTripPlans = tripPlanRepository.findByUser(currentUser);
-        List<TripPlan> tripPlans = new ArrayList<>();
-        for (Route route : routes) {
-            for (HolidayPlan holidayPlan : holidayPlans) {
-                if (checkMatchingRoutesAndTripPlans(route, holidayPlan)) {
-                    TripPlan newTripPlan = TripPlan.builder()
-                            .originIata(route.getOrigin().getIataCode())
-                            .destinationIata(route.getDestination().getIataCode())
-                            .beginDate(holidayPlan.getBeginDate())
-                            .endDate(holidayPlan.getEndDate())
-                            .adults(adults)
-                            .user(currentUser)
-                            .build();
-                    tripPlans.add(newTripPlan);
-                }
-            }
-        }
-        return tripPlans.stream()
-                .filter(tp -> !currentUserTripPlans.contains(tp))
-                .map(tp -> tripPlanRepository.save(tp))
-                .collect(Collectors.toList());
-    }
-
-    private boolean checkMatchingRoutesAndTripPlans(Route route, HolidayPlan holidayPlan) {
-        return ((route.getDaysOfWeek().contains(holidayPlan.getBeginDate().getDayOfWeek()))
-                && (route.getDaysOfWeek().contains(holidayPlan.getEndDate().getDayOfWeek())))
-                && (route.isFavourite());
-    }
-
-    public String getWeatherForTripPlanDestination(TripPlan tripPlan) {
-        try {
-            return airportService.getWeatherForAirport(airportService.getAirportByIata(tripPlan.getDestinationIata()));
-        } catch (Exception e) {
-            return "Not found";
-        }
-    }
-
-    public String getCityForTripPlanDestination(TripPlan tripPlan) {
-        try {
-            return airportService.getAirportByIata(tripPlan.getDestinationIata()).getCity();
-        } catch (Exception e) {
-            return "Not found";
-        }
     }
 }
