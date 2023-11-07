81fbe20a1e06c2199b3a914dc6475537668c277b
Seweryn <sew_k@wp.pl>
2023-11-07 20:55:11 +0100

    TripPlanServiceTestSuite: Initial version of test suite for TripPlanService class.

---
+++ src/test/java/com/kodilla/cheapflightsearch/service/TripPlanServiceTestSuite.java
@@ -0,0 +1,44 @@
+package com.kodilla.cheapflightsearch.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TripPlanServiceTestSuite {
+
+    @Test
+    void getTripPlans() {
+    }
+
+    @Test
+    void getTripPlansByUserId() {
+    }
+
+    @Test
+    void getTripPlansByUser() {
+    }
+
+    @Test
+    void deleteTripPlan() {
+    }
+
+    @Test
+    void updateTripPlan() {
+    }
+
+    @Test
+    void createTripPlan() {
+    }
+
+    @Test
+    void createTripPlansFromFavouriteRoutesAndHolidayPlans() {
+    }
+
+    @Test
+    void getWeatherForTripPlanDestination() {
+    }
+
+    @Test
+    void getCityForTripPlanDestination() {
+    }
+}
